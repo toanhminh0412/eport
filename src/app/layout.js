65545f1e9be809c3d12118e0c09a0f29bed1b26b
Archie To <toanhminh0412@gmail.com>
2023-07-13 20:21:42 -0700

    Change page padding top

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} pt-20`}>{children}</body>
+      <body className={`${inter.className} pt-16`}>{children}</body>
     </html>
   )
 }

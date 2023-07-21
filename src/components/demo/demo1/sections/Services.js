266c9fadea9652ba7a1261660a0129cd2d35b01e
Archie To <toanhminh0412@gmail.com>
2023-07-21 16:04:27 -0700

    Add this section for display services

--- src/components/demo/demo1/sections/Services.js
+++ src/components/demo/demo1/sections/Services.js
@@ -1,5 +1,16 @@
 export default function Services({content}) {
     return (
-        <h1>Services</h1>
+        <section className="prose mt-12">
+            <h1>Services</h1>
+            <div className="block sm:grid md:block lg:grid grid-cols-2 grid-rows-2 gap-4">
+                {content.services.map((service, index) => (
+                    <div key={index} className="text-center mt-2 sm:mt-0 md:mt-2 lg:mt-0">
+                        <i className={`${service.icon} text-4xl border-2 p-5 rounded-full text-blue-500 hover:text-white hover:bg-blue-500 duration-500 border-blue-500`}></i>
+                        <h3>{service.title}</h3>
+                        <p>{service.description}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }

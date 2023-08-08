f82ae6760704bff85003bdbd6ee15f75e38dc4e6
Archie To <toanhminh0412@gmail.com>
2023-08-08 15:26:44 -0700

    Prevent posting empty URL

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,4 +1,5 @@
 // Add https in front of string if string doesn't have
 export const convertToURL = url => {
+    if (!url) return '#';
     return url.includes('https') || url === '#' || url.startsWith('/') ? url : `https://${url}`;
 }

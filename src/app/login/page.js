69cd783c3097c4aae870e4bedd806ed18904b375
Archie To <toanhminh0412@gmail.com>
2023-08-08 21:09:51 -0700

    Fix issue #5: Redecorate Login/Signup page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -60,13 +60,13 @@ export default function Login() {
 
     return (
         <div>
-            <div className="flex flex-row flex-wrap text-black">
-                <div className="w-full lg:w-1/2 pt-20 lg:pt-40 pl-12 h-fit">
-                    <h1 className="font-semibold text-3xl lg:text-4xl">Welcome to Eport</h1>
-                    <p className="text-lg lg:text-xl mt-2">Build your own website. No coding or design skill needed!</p>
+            <div className="flex flex-row flex-wrap text-black pt-10 lg:pt-32">
+                <div className="w-11/12 mx-auto md:mx-0 text-center md:text-left lg:w-1/2 md:pl-12 h-fit">
+                    <h1 className="font-semibold text-3xl lg:text-4xl">Welcome to <span className="text-blue-400">Eport</span></h1>
+                    <p className="text-lg lg:text-xl mt-4 font-light">Impress employers with a professional portfolio website! Build yours now! Barely any work is needed!</p>
                 </div>
-                <div className="w-full lg:w-1/2 pt-10 lg:pt-40 pl-12 lg:px-6">
-                    <form className="card w-96 bg-white shadow-xl p-4" onSubmit={login}>
+                <div className="w-full lg:w-1/2 pt-10 lg:pt-0 md:pl-12 lg:px-6">
+                    <form className="card w-11/12 mx-auto xs:w-96 bg-white shadow-xl p-4" onSubmit={login}>
                     {errorMsg ? (
                     <div className="alert alert-error bg-red-300 border-none">
                         <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>

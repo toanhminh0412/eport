46c66e07ac5032b78a45051e65ecdf17e59acb81
Archie To <toanhminh0412@gmail.com>
2023-08-21 22:22:23 -0600

    Remove auth export for Firebase

--- public/libs/firebase.js
+++ public/libs/firebase.js
@@ -20,9 +20,6 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
-
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 

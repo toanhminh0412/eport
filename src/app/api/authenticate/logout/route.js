94d69b616c9d892a5fb14d84e3acb692877ba985
Archie To <toanhminh0412@gmail.com>
2023-08-31 13:25:09 -0700

    Enable cookie deletion on logout route

--- src/app/api/authenticate/logout/route.js
+++ src/app/api/authenticate/logout/route.js
@@ -6,9 +6,9 @@ export async function GET(request) {
     const cookieStore = cookies();
 
     // Delete user info from cookie (Log user out)
-    // cookieStore.delete('eport-uid');
-    // cookieStore.delete('eport-email');
-    // cookieStore.delete('eport-email-verified');
-    // cookieStore.delete('eport-domain');
+    cookieStore.delete('eport-uid');
+    cookieStore.delete('eport-email');
+    cookieStore.delete('eport-email-verified');
+    cookieStore.delete('eport-domain');
     redirect('/login');
 }

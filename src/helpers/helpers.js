6f0e01bcbfa26f0e64f8a58f14c114f38f9feed8
Archie To <toanhminh0412@gmail.com>
2023-08-31 17:12:41 -0700

    Prevent urls from having beggining '#'s

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,5 +1,14 @@
 // Add https in front of string if string doesn't have
 export const convertToURL = url => {
     if (!url) return '#';
+    
+    // If a string start withs one or multiple '#', remove all of the beginning '#'s
+    for (let i = 0; i < url.length; i++) {
+        if (url[i] !== '#') {
+            url = url.slice(i);
+            break;
+        }
+    }
+
     return url.includes('https') || url === '#' || url.startsWith('/') ? url : `https://${url}`;
 }

e440e267bb1214f667d4aae4d0f33a770cb968a6
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-09-01 15:01:48 -0600

    Issue #37: Build an examples page

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -7,34 +7,35 @@ export default function Footer({isLoggedIn = true}) {
         <footer className="bg-gradient-to-r from-cyan-500 to-blue-500 box-border absolute bottom-0 w-full h-fit pt-2">
             <div className="max-w-screen-xl m-auto">
                 <div className="flex flex-wrap">
-                    <div className="w-1/3 px-4 py-0 mt-10">
-                        <h3 className="text-xl text-white mb-7 relative before:content-[''] before:absolute before:left-0 before:bottom-[-10px] before:bg-red-500 before:h-[2px] before:box-border before:w-[50px]">Our Website</h3>
-                        <ul className="list-none px-0 py-20">
-                            <li className="mb-3"><Link href="/features" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Our Feature</Link></li>
-                            <li className="mb-3"><Link href="/features#pricing" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Pricing</Link></li>
-                            <li className="mb-3"><Link href="/examples" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Examples</Link></li>
+                    <div className="w-1/3 px-4 py-0 mt-8">
+                        <h3 className="text-xl text-white relative before:content-[''] before:absolute before:left-0 before:bottom-[-10px] before:bg-red-500 before:h-[2px] before:box-border before:w-[50px]">Our Website</h3>
+                        <ul className="list-none px-0 py-12">
+                            <li className="mb-2"><Link href="/features" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Our Feature</Link></li>
+                            <li className="mb-2"><Link href="/features#pricing" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Pricing</Link></li>
+                            <li className="mb-2"><Link href="/examples_premium" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Examples Premium</Link></li>
+                            <li className=""><Link href="/examples_basic" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Examples Basic</Link></li>
                         </ul>
                     </div>
                     
                     {isLoggedIn ?
-                    <div className="w-1/3 px-4 py-0 mt-10">
-                        <h3 className="text-xl text-white mb-7 relative before:content-[''] before:absolute before:left-0 before:bottom-[-10px] before:bg-red-500 before:h-[2px] before:box-border before:w-[50px]">Home</h3>
-                        <ul className="list-none px-0 py-20">
-                            <li className="mb-3"><Link href="/" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Dashboard</Link></li>
+                    <div className="w-1/3 px-4 py-0 mt-8">
+                        <h3 className="text-xl text-white relative before:content-[''] before:absolute before:left-0 before:bottom-[-10px] before:bg-red-500 before:h-[2px] before:box-border before:w-[50px]">Home</h3>
+                        <ul className="list-none px-0 py-12">
+                            <li className="mb-2"><Link href="/" className="text-lg text-slate-200 no-underline block duration-300 hover:pl-[10px] hover:text-white">Dashboard</Link></li>
                         </ul>
                     </div>
                     :null}
 
-                    <div className="w-1/3 px-4 py-0 mt-10">
-                        <h3 className="text-xl text-white mb-7 relative before:content-[''] before:absolute before:left-0 before:bottom-[-10px] before:bg-red-500 before:h-[2px] before:box-border before:w-[50px]">Follow Us</h3>
-                        <div className="px-0 py-20">
-                            <Link href="" className="inline-block h-10 w-10 text-xl bg-slate-500 mt-0 mr-6 mb-3 ml-0 text-center leading-10 rounded-full text-white duration-500 hover:text-black hover:bg-white"><i className="fa-brands fa-instagram"></i></Link>
-                            <Link href="" className="inline-block h-10 w-10 text-xl bg-slate-500 mt-0 mr-6 mb-3 ml-0 text-center leading-10 rounded-full text-white duration-500 hover:text-black hover:bg-white"><i className="fa-brands fa-facebook"></i></Link>
-                            <Link href="" className="inline-block h-10 w-10 text-xl bg-slate-500 mt-0 mr-6 mb-3 ml-0 text-center leading-10 rounded-full text-white duration-500 hover:text-black hover:bg-white"><i className="fa-brands fa-twitter"></i></Link>
+                    <div className="w-1/3 px-4 py-0 mt-8">
+                        <h3 className="text-xl text-white relative before:content-[''] before:absolute before:left-0 before:bottom-[-10px] before:bg-red-500 before:h-[2px] before:box-border before:w-[50px]">Follow Us</h3>
+                        <div className="px-0 py-12">
+                            <Link href="" className="inline-block h-10 w-10 text-xl bg-slate-500 mt-0 mr-6 mb-2 ml-0 text-center leading-10 rounded-full text-white duration-500 hover:text-black hover:bg-white"><i className="fa-brands fa-instagram"></i></Link>
+                            <Link href="" className="inline-block h-10 w-10 text-xl bg-slate-500 mt-0 mr-6 mb-2 ml-0 text-center leading-10 rounded-full text-white duration-500 hover:text-black hover:bg-white"><i className="fa-brands fa-facebook"></i></Link>
+                            <Link href="" className="inline-block h-10 w-10 text-xl bg-slate-500 mt-0 mr-6 mb-2 ml-0 text-center leading-10 rounded-full text-white duration-500 hover:text-black hover:bg-white"><i className="fa-brands fa-twitter"></i></Link>
                         </div>
                     </div>
                 </div>
-                <div className="text-white text-lg mb-5 text-center">© Copyright Eport (2023)</div>
+                <div className="text-white text-lg mb-3 text-center">© Copyright Eport (2023)</div>
             </div>
         </footer>
     )

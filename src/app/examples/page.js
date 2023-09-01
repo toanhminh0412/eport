e95d9ba02e6cadca594e9e7d91adb75969fa30cc
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-08-31 22:25:51 -0600

    Issue #37: Build examples page

--- src/app/examples/page.js
+++ src/app/examples/page.js
@@ -7,6 +7,9 @@ import Link from "next/link"
 export default function Examples() {
     const slides1 = [
         {
+            img: '/img/ckiki_profile.png'
+        },
+        {
             img: '/img/ckiki_aboutme.png'
         },
         {
@@ -34,44 +37,83 @@ export default function Examples() {
 
     const slides2 = [
         {
-            img: '/img/header-bg.jpg'
+            img: '/img/stiffany_profile.png'
         },
         {
-            img: '/img/pricing-bg.jpg'
+            img: '/img/stiffany_aboutme.png'
+        },
+        {
+            img: '/img/stiffany_skills.png'
+        },
+        {
+            img: '/img/stiffany_experience.png'
+        },
+        {
+            img: '/img/stiffany_services.png'
+        },
+        {
+            img: '/img/stiffany_projects.png'
+        },
+        {
+            img: '/img/stiffany_testimonials.png'
+        },
+        {
+            img: '/img/stiffany_references.png'
+        },
+        {
+            img: '/img/stiffany_getintouch.png'
         },
     ];
 
     const slides3 = [
         {
-            img: '/img/header-bg.jpg'
+            img: '/img/mmiles_profile.png'
         },
         {
-            img: '/img/pricing-bg.jpg'
+            img: '/img/mmiles_aboutme.png'
+        },
+        {
+            img: '/img/mmiles_experience.png'
+        },
+        {
+            img: '/img/mmiles_services.png'
+        },
+        {
+            img: '/img/mmiles_projects.png'
+        },
+        {
+            img: '/img/mmiles_testimonials.png'
+        },
+        {
+            img: '/img/mmiles_references.png'
+        },
+        {
+            img: '/img/mmiles_getintouch.png'
         },
     ];
 
     return (
         <div>
-            <div className="mb-10">
-                <div className="flex justify-center mt-12 mb-[-20px]">
+            <div className="mb-20">
+                <div className="flex justify-center mt-12">
                     <h1 className="text-3xl">Crispian Kiki - Software Developer</h1>
-                    <Link href="https://www.eport.site/ckiki" className="py-4 px-[2.5rem] rounded-16 bg-blue-500 hover:bg-blue-600 hover:shadow-none shadow-blue-btn text-xl decoration-black tracking-widest font-semibold duration-500 no-underline ml-5">Pricing</Link>
+                    <Link href='https://www.eport.site/ckiki' target="_blank" className="btn bg-blue-500 hover:bg-blue-700 duration-200 text-white ml-5 mt-[-3px]">Visit Site</Link>
                 </div>
                 <Slider slides={slides1}></Slider>
             </div>
             
-            <div className="mb-10">
-                <div className="flex justify-center mt-12 mb-[-20px]">
-                    <h1 className="text-3xl">Crispian Kiki - Software Developer</h1>
-                    <button className="btn btn-active btn-primary ml-5">Visit Site</button>
+            <div className="mb-20">
+                <div className="flex justify-center mt-12">
+                    <h1 className="text-3xl">Sabryna Tiffany - Nurse</h1>
+                    <Link href='https://www.eport.site/stiffany' target="_blank" className="btn bg-blue-500 hover:bg-blue-700 duration-200 text-white ml-5">Visit Site</Link>
                 </div>
                 <Slider slides={slides2}></Slider>
             </div>
 
             <div className="mb-20">
-                <div className="flex justify-center mt-12 mb-[-20px]">
-                    <h1 className="text-3xl">Crispian Kiki - Software Developer</h1>
-                    <button className="btn btn-active btn-primary ml-5">Visit Site</button>
+                <div className="flex justify-center mt-12">
+                    <h1 className="text-3xl">Maxine Miles - Senior Chef</h1>
+                    <Link href='https://www.eport.site/mmiles' target="_blank" className="btn bg-blue-500 hover:bg-blue-700 duration-200 text-white ml-5">Visit Site</Link>
                 </div>
                 <Slider slides={slides3}></Slider>
             </div>

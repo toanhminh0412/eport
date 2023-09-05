5b7a049ee327a8da1eb9f993c5308da45f1cea12
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-09-04 23:30:57 -0600

    Issue #67: Support login with other platforms

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import secureLocalStorage from "react-secure-storage";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../../public/libs/firebase";
 
 export default function Login() {
     const [email, setEmail] = useState('');
@@ -53,6 +55,50 @@ export default function Login() {
         })
     }
 
+    // const auth = auth();
+    const signInWithGoogle = () => {
+        const provider = new GoogleAuthProvider();
+
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user);
+            setLoading(true);
+            fetch(`/api/authenticate/signin_with_google`, {
+                method: 'GET',
+                headers: {
+                    'X-forwarded-email' : user.email,
+                    'X-forwarded-uid': user.uid,
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    secureLocalStorage.setItem('eport-uid', data.uid);
+                    secureLocalStorage.setItem('eport-email', data.email);
+                    secureLocalStorage.setItem('eport-signInMethod', data.signInMethod);
+                    window.location.href = "/";
+                } else {
+                    setLoading(false);
+                    setErrorMsg(data.message);
+                    setTimeout(() => {
+                        setErrorMsg('');
+                    }, 5000);
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                setLoading(false);
+            })
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+    
+
     return (
         <div>
             <div className="flex flex-row flex-wrap text-black pt-10 lg:pt-32 mb-32">
@@ -62,23 +108,24 @@ export default function Login() {
                 </div>
                 <div className="w-full lg:w-1/2 pt-10 lg:pt-0 md:pl-12 lg:px-6">
                     <form className="card w-11/12 mx-auto xs:w-96 bg-white shadow-xl p-4" onSubmit={login}>
-                    {errorMsg ? (
-                    <div className="alert alert-error bg-red-300 border-none">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                        <span>{errorMsg}</span>
-                    </div>
-                    ) : null}
-                    <label className="label">
-                        <span className="label-text">Email:</span>
-                    </label>
-                    <input type="email" placeholder="Email" className="input input-bordered w-full" onChange={updateEmail}/>
-                    <label className="label mt-2">
-                        <span className="label-text">Password:</span>
-                    </label>
-                    <input type="password" placeholder="Password" className="input input-bordered w-full" onChange={updatePassword}/>
-                    <Link href="/forgotpassword" className="link text-blue-700 mt-4">Forgot password?</Link>
-                    <p className="mt-2">Don&apos;t have an account? <Link href="/signup" className="link text-blue-700">Sign up</Link>!</p>
-                    <input type="submit" value={loading? "Logging in..." : "Login"} className="btn w-fit mt-6 bg-orange-600 hover:bg-orange-800 text-white" disabled={!(email&&password) || loading}></input>
+                        {errorMsg ? (
+                        <div className="alert alert-error bg-red-300 border-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                            <span>{errorMsg}</span>
+                        </div>
+                        ) : null}
+                        <label className="label">
+                            <span className="label-text">Email:</span>
+                        </label>
+                        <input type="email" placeholder="Email" className="input input-bordered w-full" onChange={updateEmail}/>
+                        <label className="label mt-2">
+                            <span className="label-text">Password:</span>
+                        </label>
+                        <input type="password" placeholder="Password" className="input input-bordered w-full" onChange={updatePassword}/>
+                        <Link href="/forgotpassword" className="link text-blue-700 mt-4">Forgot password?</Link>
+                        <p className="mt-2">Don&apos;t have an account? <Link href="/signup" className="link text-blue-700">Sign up</Link>!</p>
+                        <input type="submit" value={loading? "Logging in..." : "Login"} className="btn w-fit mt-6 bg-orange-600 hover:bg-orange-800 text-white" disabled={!(email&&password) || loading}></input>
+                        <div className="btn mt-6" onClick={() => signInWithGoogle()}><i className="fa-brands fa-google"></i>Sign in with Google</div>
                     </form>
                 </div>
             </div>

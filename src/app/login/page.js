a11d7e19e0828fe04c26e99090b11da9e8ff7ed5
Hoang Nguyen <74847239+Bibo1604@users.noreply.github.com>
2023-09-06 17:04:30 -0600

    Issue #67: Support login with other platforms

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -56,7 +56,7 @@ export default function Login() {
         })
     }
 
-    // const auth = auth();
+    // Sign In With Google
     const signInWithGoogle = () => {
         const provider = new GoogleAuthProvider();
 

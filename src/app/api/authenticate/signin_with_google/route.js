85d55586925dc5d04babd4379b62bf12c46f3774
Archie To <toanhminh0412@gmail.com>
2023-09-08 14:48:45 -0700

    Issue 35: Get user current plan when signing in with Google

--- src/app/api/authenticate/signin_with_google/route.js
+++ src/app/api/authenticate/signin_with_google/route.js
@@ -17,6 +17,7 @@ export async function GET(request) {
     const usersEmail = query(usersCollection, where ("email", "==", email))
     const usersDocs = await getDocs(usersEmail);
 
+    // Didn't find any user with this Google email
     if (usersDocs.docs.length === 0) {
         const newUserId = nanoid();
         const newUser = {
@@ -37,25 +38,46 @@ export async function GET(request) {
         cookieStore.set('eport-signInMethod', newUser.signInMethod);
         cookieStore.set('eport-email-verified', newUser.emailVerified);
         cookieStore.set('eport-domain', newUser.domain);
+        cookieStore.set('eport-stripe-customer-id', '');
 
         success = true;
         message = 'Login successfully!';
+
+    // Found an user with this Google email
     } else {
-        usersDocs.forEach((doc) => {
-            const user = doc.data();
-            if (user.signInMethod === "Google") {
-                success = true;
-                message = 'Login successfully!';
-                cookieStore.set('eport-uid', user.uid);
-                cookieStore.set('eport-email', user.email);
-                cookieStore.set('eport-signInMethod', 'Google');
-                cookieStore.set('eport-email-verified', true);
-                cookieStore.set('eport-domain', "");
+        const user = usersDocs.docs[0].data();
+        if (user.signInMethod === "Google") {
+            success = true;
+            message = 'Login successfully!';
+            cookieStore.set('eport-uid', user.uid);
+            cookieStore.set('eport-email', user.email);
+            cookieStore.set('eport-signInMethod', 'Google');
+            cookieStore.set('eport-email-verified', true);
+            cookieStore.set('eport-domain', "");
+            cookieStore.set('eport-stripe-customer-id', user.stripeCustomerId ? user.stripeCustomerId : '');
+
+            // Check if user has an active subscription
+            const stripeCustomerId = cookieStore.get('eport-stripe-customer-id') ? cookieStore.get('eport-stripe-customer-id').value : '';
+            if (!user.stripeCustomerId) {
+                cookieStore.set('eport-plan', 'basic');
             } else {
-                success = false;
-                message = "Google SignIn option is not available to this email. Please log in using your email and password!";
+                const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+                const customer = await stripe.customers.retrieve(
+                    stripeCustomerId, {
+                        expand: ['subscriptions']
+                    }
+                );
+                if (customer && customer.subscriptions.data.length > 0 && customer.subscriptions.data[0].current_period_end * 1000 > new Date().getTime()) {
+                    console.log('Subscription is active');
+                    cookieStore.set('eport-plan', 'premium');
+                } else {
+                    cookieStore.set('eport-plan', 'basic');
+                }
             }
-        });
+        } else {
+            success = false;
+            message = "Google SignIn option is not available to this email. Please log in using your email and password!";
+        }
     }
 
     return NextResponse.json({

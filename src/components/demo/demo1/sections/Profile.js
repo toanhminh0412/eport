75c083a98791a9e18fe16f2bf4fc4875a17c445e
Archie To <toanhminh0412@gmail.com>
2023-09-20 11:59:26 -0700

    Issue #119: Remove unwanted code

--- src/components/demo/demo1/sections/Profile.js
+++ src/components/demo/demo1/sections/Profile.js
@@ -4,28 +4,6 @@ import Link from "next/link";
 export default function Profile({content}) {
     const section = content;
 
-    // return (
-    //     <div className="w-full bg-black h-96">
-    //         <div className="card-body p-8 text-center">
-    //             <Image 
-    //             src={section.profilePic} 
-    //             alt="Profile picture" 
-    //             width={250} 
-    //             height={250} 
-    //             style={{objectFit: "contain"}}
-    //             className="mx-auto"/>
-    //             <h1 className="font-bold text-4xl mt-4">{section.fullName}</h1>
-    //             <div className="text-2xl font-normal text-slate-500">{section.job}</div>
-    //             {section.cvURL ? 
-    //             <Link
-    //                 href={section.cvURL} 
-    //                 className="btn bg-blue-500 hover:bg-blue-700 duration-200 text-white mt-8 w-full max-w-xs mx-auto" 
-    //                 target="_blank"
-    //                 prefetch={false}>View CV</Link> : null}
-    //         </div>
-    //     </div>
-    // )
-
     return (
         <div className="w-11/12 mx-auto shadow-md">
             <div className="hero h-40 xs:h-60 lg:h-80 relative rounded-t-lg" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>

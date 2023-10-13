b12e994ac1635029a6e1aabfbb761b0bffd3b8b3
Archie To <toanhminh0412@gmail.com>
2023-10-12 22:35:12 -0700

    Issue #144: Latest version

--- src/components/freelancer/template1/sections/editable/EditableAboutMes.js
+++ src/components/freelancer/template1/sections/editable/EditableAboutMes.js
@@ -23,17 +23,17 @@ export function EditableAboutMe1({ section, sectionInd }) {
                     <h3 className="font-bold text-lg">Delete Section!</h3>
                     <p className="py-4">Are you sure you want to delete this {section.sectionType} section?</p>
                     <div className="modal-action">
-                        <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
+                        {/* if there is a button in form, it will close the modal */}
+                        <form className="dialog">
                             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                             <button className="btn mr-4 bg-blue-700 hover:bg-blue-900 duration-200 text-white" onClick={() => deleteSection(section)}>Yes</button>
-                            <button className="btn mr-[-50px] bg-red-700 hover:bg-red-900 duration-200 text-white">No</button>
-                        </form>
-                        <form method="dialog" className="modal-backdrop">
-                            <button>close</button>
+                            <button className="btn bg-red-700 hover:bg-red-900 duration-200 text-white">No</button>
                         </form>
                     </div>
                 </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
             </dialog>
             <section className={`block bg-white box-border border-4 ${activeSectionInd === sectionInd ? "border-blue-700" : "border-transparent"} hover:border-blue-700 duration-200`} onClick={openContentTabEditor}>
                 <div className="py-12 md:py-16 lg:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">

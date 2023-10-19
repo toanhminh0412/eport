422c7a4d9d276e2116aeb4b83a67370f289814f7
Archie To <toanhminh0412@gmail.com>
2023-10-19 16:33:30 -0700

    Add an alert to publish modal

--- src/components/ui/PublishModal.js
+++ src/components/ui/PublishModal.js
@@ -148,6 +148,12 @@ export default function PublishModal({site, projectId, publishedSite=null, showM
                 <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 <h3 className="font-bold text-lg">Publish site</h3>
 
+                {/* Display reminder */}
+                {projectType === "freelancer" ? <div className="alert alert-info my-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span><strong>Reminder: </strong>You need to click "Save" before publishing to see your latest changes</span>
+                </div> : null}
+
                 {/* Message display */}
                 {error ?
                 <div className="alert alert-error mt-3">

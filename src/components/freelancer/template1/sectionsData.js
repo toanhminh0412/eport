bae21c6b802bdb7f8c0a2881bb01968493d5be02
Archie To <toanhminh0412@gmail.com>
2023-10-25 11:55:21 -0700

    Update thumbnail images for new sections of template 1

--- src/components/freelancer/template1/sectionsData.js
+++ src/components/freelancer/template1/sectionsData.js
@@ -9,19 +9,19 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "navbar2",
             sectionType: "navbar",
-            thumbnail: "/img/freelancer-template1-navbar1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-navbar2-thumbnail.png",
             description: "Template 1 navigation bar 2"
         },
         {
             sectionId: "navbar3",
             sectionType: "navbar",
-            thumbnail: "/img/freelancer-template1-navbar1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-navbar3-thumbnail.png",
             description: "Template 1 navigation bar 3"
         },
         {
             sectionId: "navbar4",
             sectionType: "navbar",
-            thumbnail: "/img/freelancer-template1-navbar1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-navbar4-thumbnail.png",
             description: "Template 1 navigation bar 4"
         }
     ],
@@ -35,7 +35,7 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "header2",
             sectionType: "header",
-            thumbnail: "/img/freelancer-template1-header1.png",
+            thumbnail: "/img/freelancer-template1-header2-thumbnail.png",
             description: "Template 1 header 2"
         }
     ],
@@ -49,7 +49,7 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "aboutme2",
             sectionType: "aboutme",
-            thumbnail: "/img/freelancer-template1-aboutme1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-aboutme2-thumbnail.png",
             description: "Template 1 aboutme 2"
         }
     ],
@@ -57,13 +57,13 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "service1",
             sectionType: "service",
-            thumbnail: "/img/freelancer-template1-service1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-service1-new-thumbnail.png",
             description: "Template 1 service 1"
         },
         {
             sectionId: "service2",
             sectionType: "service",
-            thumbnail: "/img/freelancer-template1-service1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-service2-thumbnail.png",
             description: "Template 1 service 2"
         }
     ],
@@ -77,7 +77,7 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "portfolio2",
             sectionType: "portfolio",
-            thumbnail: "/img/freelancer-template1-portfolio1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-portfolio2-thumbnail.png",
             description: "Template 1 portfolio 2"
         }
     ],
@@ -91,7 +91,7 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "testimonial2",
             sectionType: "testimonial",
-            thumbnail: "/img/freelancer-template1-testimonial1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-testimonial2-thumbnail.png",
             description: "Template 1 testimonial 2"
         }
     ],
@@ -105,7 +105,7 @@ const sectionsDataTemplate1 = {
         {
             sectionId: "contact2",
             sectionType: "contact",
-            thumbnail: "/img/freelancer-template1-contact1-thumbnail.png",
+            thumbnail: "/img/freelancer-template1-contact2-thumbnail.png",
             description: "Template 1 contact 2"
         }
     ]

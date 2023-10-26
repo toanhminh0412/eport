ebb8a7904638ea1f979cc5d02748d2008b3bbfa3
Archie To <toanhminh0412@gmail.com>
2023-10-25 20:03:31 -0700

    Remove the padding bottom on published sites

--- src/components/freelancer/template0/published.js
+++ src/components/freelancer/template0/published.js
@@ -4,7 +4,7 @@ export default function PublishedTemplate0({project}) {
     const publish = true;
     const isNavbarUsed = project.sections.some(section => section.sectionType === "navbar");
     return (
-        <main className={`w-full relative ${isNavbarUsed ? "" : "mt-[-65px]"}`}>
+        <main className={`w-full relative mb-[-400px] xs:mb-[-340px] ${isNavbarUsed ? "" : "mt-[-65px]"}`}>
             {project.sections.map(section => <div key={section.id} id={section.id} style={section.sectionType !== "navbar" ? {zoom: "75%"} : {}}><Section section={section} publish={publish} ownerEmail={project.ownerEmail}/></div>)}
         </main>
     )
--- src/components/freelancer/template1/published.js
+++ src/components/freelancer/template1/published.js
@@ -5,7 +5,7 @@ export default function PublishedTemplate1({project}) {
     const publish = true;
     const isNavbarUsed = project.sections.some(section => section.sectionType === "navbar");
     return (
-        <main className={`w-full relative ${isNavbarUsed ? "mt-1" : "mt-[-65px]"}`}>
+        <main className={`w-full relative mb-[-400px] xs:mb-[-340px] ${isNavbarUsed ? "mt-1" : "mt-[-65px]"}`}>
             {project.sections.map(section => <div key={section.id} id={section.id} style={section.sectionType !== "navbar" ? {zoom: "75%"} : {}}><Section section={section} publish={publish} ownerEmail={project.ownerEmail}/></div>)}
         </main>
     )
